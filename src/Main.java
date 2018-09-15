3e8d9ad5df052fe88248a137f017b6f4b7dbf7a7
gurpreeth956 <gurpreeth956@gmail.com>
2018-09-14 20:52:16 -0400

    Working on player class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -8,7 +10,8 @@ public class Main extends Application {
 
     Stage stage;
     Scene scene;
-    static Pane gameArea;
+    static Pane gameRoot;
+    static BorderPane menuRoot;
     
     public static void main(String[] args) {
         launch(args);
@@ -17,19 +20,31 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         stage = primaryStage;
-        gameArea = new Pane();
-        scene = new Scene(gameArea, 200, 200);
+        gameRoot = new Pane();
+        menuRoot = new BorderPane();
+        scene = new Scene(menuRoot, 200, 200);
         
         Button bttn = new Button("Start");
         bttn.setOnAction(e -> {
-            stage.close();
+            stage.getScene().setRoot(menuRoot);
         });
         
-        gameArea.getChildren().add(bttn);
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                update(stage);
+            }
+        };
+        timer.start();
+        
+        menuRoot.setCenter(bttn);
         stage.setTitle("The Elimination of Space Pollution");
         stage.setScene(scene);
         stage.setFullScreen(true);
         stage.show();
     }
-
+    
+    public void update(Stage stage) {
+        
+    }
 }
---
+++ src/Player.java
@@ -0,0 +1,17 @@
+
+import javafx.scene.image.ImageView;
+
+public class Player {
+    
+    ImageView iv;
+    int offsetX = 0;
+    int offsetY = 0;
+    int width = 0;
+    int height = 0;
+    
+    int x; //Character xPos
+    int y; //Character yPos
+    
+    int health = 5;
+    boolean alive = true;
+}
