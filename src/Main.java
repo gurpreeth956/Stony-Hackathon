e6d219cbc93cdf238a5184d74ec2579cecab2ee3
gurpreeth956 <gurpreeth956@gmail.com>
2018-09-14 20:06:29 -0400

    Start of SBU Hackathon Project

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    Stage stage;
+    Scene scene;
+    static Pane gameArea;
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        stage = primaryStage;
+        gameArea = new Pane();
+        scene = new Scene(gameArea, 200, 200);
+        
+        Button bttn = new Button("Start");
+        bttn.setOnAction(e -> {
+            stage.close();
+        });
+        
+        gameArea.getChildren().add(bttn);
+        stage.setTitle("The Elimination of Space Pollution");
+        stage.setScene(scene);
+        stage.setFullScreen(true);
+        stage.show();
+    }
+
+}
